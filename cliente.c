0f4c22351570e7da773d308498c0ed58e410442e
CristianoCamposvr <113101828+CristianoCamposvr@users.noreply.github.com>
2023-10-24 14:37:28 -0300

    Create cliente.c

---
+++ cliente.c
@@ -0,0 +1,27 @@
+/*
+
+  cliente.c - Aplicação principal que utiliza as estruturas de dados 'árvore binária de busca' e 'pilha',
+              simulando um cenário de aventura em uma floresta misteriosa, onde estratégias de sobrevivência
+              contra monstros são testadas, como parte das atividades do Laboratório 5 de MC202G+H.
+
+  Autor: Cristiano Campos e Heigon Soldera.
+
+  MC202G+H - 2º semestre de 2023.
+
+  Última alteração: 19/09/2023.
+
+*/
+
+#include "ficha.h"
+
+
+int main ()
+{
+  return 0;
+}
+
+
+
+/*
+   Fim do programa
+*/
