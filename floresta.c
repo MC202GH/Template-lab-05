5f36b59860eb72c6069ccce4f2b37377a483de26
CristianoCamposvr <113101828+CristianoCamposvr@users.noreply.github.com>
2023-10-24 14:32:56 -0300

    Create floresta.c

---
+++ floresta.c
@@ -0,0 +1,21 @@
+/*
+
+  floresta.c - Implementação do TAD para manipulação de uma árvore de busca binária (BST) 
+               e pilha, utilizados no contexto de um jogo em uma floresta de monstros.
+
+  Autor: Cristiano Campos e Heigon Soldera.
+
+  MC202G+H - 2 semestre de 2023.
+
+  Última alteração: 24/10/2023.
+
+*/
+
+#include "floresta.h"
+
+/* TODO: implemente aqui as funções da interface! */
+
+
+/*
+   Fim do arquivo de implementação.
+*/
